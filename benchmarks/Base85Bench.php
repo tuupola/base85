cbfb78dede6a8f2f5d44e115cd0993d6d2a23311
Mika Tuupola <tuupola@appelsiini.net>
2017-02-25 21:25:11 +0700

    Add basic benchmarks

---
+++ benchmarks/Base85Bench.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the Base85 package
+ *
+ * Copyright (c) 2017 Mika Tuupola
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * Project home:
+ *   https://github.com/tuupola/base85
+ *
+ */
+
+use Tuupola\Base85\PhpEncoder;
+use Tuupola\Base85\GmpEncoder;
+
+/**
+ * @BeforeMethods({"init"})
+ */
+
+class Base85Bench
+{
+    private $data;
+
+    public function init()
+    {
+        $this->data = random_bytes(128);
+        $this->gmp = new GmpEncoder;
+        $this->php = new PhpEncoder;
+    }
+
+    /**
+     * @Revs(10)
+     */
+    public function benchGmpEncoder()
+    {
+        $encoded = $this->gmp->encode($this->data);
+        $decoded = $this->gmp->decode($encoded);
+    }
+
+    /**
+     * @Revs(10)
+     */
+    public function benchPhpEncoder()
+    {
+        $encoded = $this->php->encode($this->data);
+        $decoded = $this->php->decode($encoded);
+    }
+}
